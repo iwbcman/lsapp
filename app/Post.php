05d1e9f291f8a57956ded54a535178e93c140666
Karl Zollner <iwbcman@watachi.com>
2021-07-30 07:43:20 -0400

    First Commit

---
+++ app/Post.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Post extends Model
+{
+    //table name
+    //protected $table = 'posts';
+    //primary key
+    //public $primaryKey = 'id';
+    // disable timestamps
+    //public $timestamps = false;
+}
