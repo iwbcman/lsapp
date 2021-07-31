13683a578dc9e10e6944d38b762701b0012be4b9
Karl Zollner <iwbcman@gmail.com>
2021-07-31 14:34:02 -0400

    redo messages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -89,7 +89,8 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main class="py-4 container">
+            @include('inc.messages')
             @yield('content')
         </main>
     </div>
